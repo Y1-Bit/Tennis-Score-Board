bbc741df5e0068e9aa58ae4420fc783cd6f82610
Y1-Bit <y0749701@gmail.com>
2024-07-30 21:08:49 +0300

    Uncomment lines

--- app/middleware/db_middleware.py
+++ app/middleware/db_middleware.py
@@ -5,10 +5,8 @@ from app.database import get_db
 
 def db_session_middleware(app: Callable) -> Callable:
     def middleware(environ, start_response):
-        print("Middleware Called")
-        # with get_db() as db:
-        #     environ["db_session"] = db
-        #     return app(environ, start_response)
-        return app(environ, start_response)
+        with get_db() as db:
+            environ["db_session"] = db
+            return app(environ, start_response)
 
     return middleware
