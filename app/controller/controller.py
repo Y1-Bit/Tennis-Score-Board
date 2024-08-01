c4ee1c2da3665839ce5bace20fcce0f225d009c3
Y1-Bit <y0749701@gmail.com>
2024-08-01 12:50:07 +0300

    Update controller for handling POST requests

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,7 +1,7 @@
 from typing import Callable
 from app.decorators import Router
 from jinja2 import Environment
-
+from urllib.parse import parse_qs
 
 class AppController:
     def __init__(self, router: Router, template_env: Environment) -> None:
@@ -16,7 +16,15 @@ class AppController:
         method = environ.get("REQUEST_METHOD", "GET")
         path = environ.get("PATH_INFO", "/")
         handler = self.router.find_handler(method, path)
+        
         if handler:
-            return handler(start_response, self.template_env)
+            db_session  = environ["db_session"]
+            if method == "POST":
+                content_length = int(environ.get("CONTENT_LENGTH", 0))
+                body = environ["wsgi.input"].read(content_length).decode()
+                form_data = parse_qs(body)
+                return handler(start_response, self.template_env, db_session, form_data)
+            else:
+                return handler(start_response, self.template_env, db_session)
         else:
             return self.not_found(start_response)
