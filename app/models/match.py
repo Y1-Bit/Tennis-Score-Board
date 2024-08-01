b16ec550b68f8d35517e303ac1569090010a2a06
Y1-Bit <y0749701@gmail.com>
2024-08-01 12:50:19 +0300

    Added __init__ method for Match model

--- app/models/match.py
+++ app/models/match.py
@@ -1,17 +1,27 @@
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, String, ForeignKey, JSON
+import uuid
+
+from sqlalchemy import JSON, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.database.database import Base
 
 
-class Matches(Base):
+class Match(Base):
     __tablename__ = "matches"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     uuid: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     player1_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
     player2_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
-    winner_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
-    score: Mapped[dict] = mapped_column(JSON, nullable=False)
+    winner_id: Mapped[int | None] = mapped_column(Integer, ForeignKey("players.id"), nullable=True)
+    score: Mapped[dict | None] = mapped_column(JSON, nullable=True)
+
+    def __init__(self, player1_id: int, player2_id: int) -> None:
+        self.uuid = str(uuid.uuid4())
+        self.player1_id = player1_id
+        self.player2_id = player2_id
+        self.score = None
+        self.winner_id = None  
 
     def __repr__(self) -> str:
         return f"<Match(id={self.id}, uuid='{self.uuid}', player1_id={self.player1_id}, player2_id={self.player2_id}, winner_id={self.winner_id}, score={self.score})>"
