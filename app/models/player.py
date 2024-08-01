47545f891b66de0721861c27bb5d2dab2abcdfa7
Y1-Bit <y0749701@gmail.com>
2024-08-01 11:40:17 +0300

    Create Matches Model

--- app/models/player.py
+++ app/models/player.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, ForeignKey, JSON
 from app.database.database import Base
 
 
@@ -10,3 +10,17 @@ class Player(Base):
         Integer, primary_key=True, index=True, autoincrement=True
     )
     name: Mapped[str] = mapped_column(String(255), nullable=False)
+
+
+class Matches(Base):
+    __tablename__ = "matches"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    uuid: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    player1_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
+    player2_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
+    winner_id: Mapped[int] = mapped_column(Integer, ForeignKey("players.id"))
+    score: Mapped[dict] = mapped_column(JSON, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Match(id={self.id}, uuid='{self.uuid}', player1_id={self.player1_id}, player2_id={self.player2_id}, winner_id={self.winner_id}, score={self.score})>"
