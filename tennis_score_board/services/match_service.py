57c09cd8a7186a177d0ad518fd6e64851a42453d
Y1-Bit <y0749701@gmail.com>
2024-08-12 13:47:35 +0300

    Removed unused import

--- tennis_score_board/services/match_service.py
+++ tennis_score_board/services/match_service.py
@@ -1,6 +1,4 @@
-from tennis_score_board.domain.match import Match as DomainMatch
-from tennis_score_board.domain.match import MatchList as DomainMatchList
-from tennis_score_board.models.match import Match as DBMatch
+from tennis_score_board.domain.match import Match, MatchList
 from tennis_score_board.services.interfaces import (
     MatchRepoInterface,
     PlayerRepoInterface,
@@ -14,14 +12,14 @@ class MatchService:
         self.match_repo = match_repo
         self.player_repo = player_repo
 
-    def create_match(self, player1_name: str, player2_name: str) -> DomainMatch:
+    def create_match(self, player1_name: str, player2_name: str) -> Match:
         player1 = self.player_repo.get_or_create(player1_name)
         player2 = self.player_repo.get_or_create(player2_name)
-        new_match = DomainMatch.create(player1.id, player2.id)
+        new_match = Match.create(player1.id, player2.id)
         return self.match_repo.add(new_match)
 
-    def list_matches(self) -> DomainMatchList:
+    def list_matches(self) -> MatchList:
         return self.match_repo.get_all()
 
-    def get_match(self, match_id: int) -> DomainMatch:
+    def get_match(self, match_id: int) -> Match:
         return self.match_repo.get_by_id(match_id)
