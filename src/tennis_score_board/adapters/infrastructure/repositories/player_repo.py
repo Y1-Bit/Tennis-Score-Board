2fe1852b947c76f5a6a8d891a75b5ea40e3e856d
Y1-Bit <y0749701@gmail.com>
2024-08-23 18:47:57 +0300

    Fixed ID issue

--- src/tennis_score_board/adapters/infrastructure/repositories/player_repo.py
+++ src/tennis_score_board/adapters/infrastructure/repositories/player_repo.py
@@ -16,6 +16,7 @@ class PlayerRepo(PlayerRepoInterface):
         if db_player is None:
             db_player = DBPlayer(name=player_name)
             self.db_session.add(db_player)
+            self.db_session.flush()  
         return self._to_domain(db_player)
 
     def _to_domain(self, db_player: DBPlayer) -> DomainPlayer:
